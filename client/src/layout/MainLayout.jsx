6c907332d9abf6bbc0912984ee25098197c87130
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-01 11:44:44 +0530

    place holder navbar add

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="bg-black">
+    <div>
       {/* Navbar */}
+      <Navbar />
 
       {/* Outlet */}
       <div>
